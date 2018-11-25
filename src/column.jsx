1b56349e635195943cfb85379447ac3a606af3ac
Saul Ocampo <digitalh2o@gmail.com>
2018-11-25 14:17:22 -0600

    new columns and moving between

--- src/column.jsx
+++ src/column.jsx
@@ -7,12 +7,20 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h3`
   padding: 8px;
 `;
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.2 ease;
+  background-color: ${props => props.isDraggingOver ? 'skyblue' : 'white'};
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 class Column extends Component {
@@ -21,10 +29,11 @@ class Column extends Component {
       <Container>
         <Title>{this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-          {(provided) => (
+          {(provided, snapshot) => (
            <TaskList
             ref={provided.innerRef}
             {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
            >
               {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index}/>)}
               {provided.placeholder}
